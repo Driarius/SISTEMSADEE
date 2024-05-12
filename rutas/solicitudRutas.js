b1195d0ea53e39f07a1d7bc1a1801f3fee43e165
Gilmar Chinche (Driarius) <gilmar.chinche@uab.edu.bo>
2024-05-12 11:44:53 -0400

    CRUD y consultas proyecto SADEE

--- rutas/solicitudRutas.js
+++ rutas/solicitudRutas.js
@@ -12,4 +12,105 @@ rutas.get('/getSolicitud', async (req, res) => {
     }
 });
 
+// endpoint 2 crear
+rutas.post('/crear', async (req, res) => {
+    const solicitud = new SolicitudModel({
+        tipo: req.body.tipo,
+        caracteristicas: req.body.caracteristicas
+    
+    })
+    try {
+        const nuevaSolicitud = await solicitud.save();
+        res.status(201).json(nuevaSolicitud);
+    } catch (error){
+        res.status(400).json({ mensaje : error.message})
+    }
+    
+});
+
+// endpoint 3 editar
+rutas.put('/editar/:id', async (req, res) => {
+    try{
+        const solicitudEditada = await SolicitudModel.findByIdAndUpdate(req.params.id, req.body, {new : true});
+        if (!solicitudEditada)
+            return res.status(404).json({ mensaje : 'Solicitud no Encontrada !!!'});
+        else
+        return res.json(solicitudEditada);
+
+    } catch (error) {
+        res.status(400).json({ mensaje : error.message});
+    }
+})
+
+// endpoint 4 eliminar
+rutas.delete('/eliminar/:id', async (req, res) => {
+    try {
+        const solicitudEliminada = await SolicitudModel.findByIdAndDelete(req.params.id);
+        if (!solicitudEliminada)
+            return res.status(404).json({mensaje : 'Solicitud no encontrada !!'});
+        else
+            return res.json({mensaje : 'Receta Eliminada'});
+        }
+        catch (error){
+            res.status(500).json({mensaje : error.message})
+        }
+});
+
+
+
+// obtener una receta por su ID
+rutas.get('/busquedaid/:id', async (req, res) =>{
+    try{
+        const busquedaid = await SolicitudModel.findById(req.params.id);
+        if(!busquedaid)
+            return res.status(404).json({mensaje : 'Solicitud no encontrada!!!'});
+        else
+        return res.json(busquedaid); 
+    } catch (error){
+        res.status(500).json({ mensaje: error.message})
+    }
+
+
+});
+// obtener recetas por un ingrediente especifico
+rutas.get ('/solicitudPorcaracteristica/:caracteristica', async(req, res) =>{
+    try{
+        const solicitudCaracteristica = await SolicitudModel.find({caracteristicas : new RegExp( req.params.caracteristica, 'i')});
+        return res.json(solicitudCaracteristica);
+    }catch (error){
+        res.status(500).json({mensaje : error.message})
+    }
+});
+// eliminar todas las receta
+rutas.delete('/eliminartodas', async (req, res) => {
+    try{
+        await SolicitudModel.deleteMany({});
+        return res.json({mensaje: "Todas las Solicitudeas Han sido eliminadas"});
+    }catch (error) {
+        res.status(500).json({mensaje : error.message})
+    }
+
+});
+
+// contar el numero total de recetas
+rutas.get('/totalSolicitudes', async (req, res) => {
+    try{
+        const total = await SolicitudModel.countDocuments();
+        return res.json({totalSolicitudes : total});
+    }catch (error){
+            res.status(500).json({mensaje : error.message});
+    }
+});
+// obtener recetas ordenadas por nombre ascendente
+rutas.get('/ordenarSolicitudes', async (req, res)=>{
+    try{
+        const solicitudesOrdenadas = await SolicitudModel.find().sort({tipo: 1 });
+        res.status(200).json(solicitudesOrdenadas);
+    }catch (error){
+        res.status(500).json({mensaje : error.message});
+    }
+
+});
+// obtener receta por cantidad
+
 module.exports = rutas;
